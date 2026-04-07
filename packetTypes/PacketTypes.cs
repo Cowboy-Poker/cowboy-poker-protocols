e54340cbd9549afc13ffe954c12da498e28ed635
김동헌 <znfnfns0365c@gmail.com>
2026-04-07 15:13:07 +0900

    ✨ update: 언어별 packetType 생성

---
+++ packetTypes/PacketTypes.cs
@@ -0,0 +1,25 @@
+public enum PacketType : ushort
+{
+    // login 100
+    C_Login  = 100,
+    S_Login  = 101,
+    C_Register = 102,
+    S_Register = 103,
+
+    // poker 200
+    C_GetRoomList       = 200,
+    S_GetRoomList       = 201,
+    C_CreateRoom        = 202,
+    S_CreateRoom        = 203,
+    C_JoinRoom          = 204,
+    S_JoinRoom          = 205,
+    C_LeaveRoom         = 206,
+    S_LeaveRoom         = 207,
+    C_StartGame         = 208,
+    S_DealCards         = 209,
+    C_BetAction         = 210,
+    S_BetTurn           = 211,
+    S_BetResult         = 212,
+    S_OpenCommunityCards = 213,
+    S_GameResult        = 214,
+}
---
+++ packetTypes/packetTypes.h
@@ -0,0 +1,27 @@
+#pragma once
+
+enum class PacketType : uint16_t
+{
+    // login 100
+    C_LOGIN  = 100,
+    S_LOGIN  = 101,
+    C_REGISTER = 102,
+    S_REGISTER = 103,
+
+    // poker 200
+    C_GET_ROOM_LIST      = 200,
+    S_GET_ROOM_LIST      = 201,
+    C_CREATE_ROOM        = 202,
+    S_CREATE_ROOM        = 203,
+    C_JOIN_ROOM          = 204,
+    S_JOIN_ROOM          = 205,
+    C_LEAVE_ROOM         = 206,
+    S_LEAVE_ROOM         = 207,
+    C_START_GAME         = 208,
+    S_DEAL_CARDS         = 209,
+    C_BET_ACTION         = 210,
+    S_BET_TURN           = 211,
+    S_BET_RESULT         = 212,
+    S_OPEN_COMMUNITY_CARDS = 213,
+    S_GAME_RESULT        = 214,
+};
